746392db21714b05ddba883c360706e7e9eab11b
zaka-invozone <zaka-invozone@users.noreply.github.com>
2021-11-02 11:09:59 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.0c0e3af54fdf91f64031f047e3712b67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93e3b9113bcf367743ca8549b5d9865",
+    "url": "https://zaka-invozone.github.io/react/build-test-deploy/index.html"
+  },
+  {
+    "revision": "c29131a9f10db34d710a",
+    "url": "https://zaka-invozone.github.io/react/build-test-deploy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3f09c34feab0c3fc16f3",
+    "url": "https://zaka-invozone.github.io/react/build-test-deploy/static/js/2.214e1e36.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "https://zaka-invozone.github.io/react/build-test-deploy/static/js/2.214e1e36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c29131a9f10db34d710a",
+    "url": "https://zaka-invozone.github.io/react/build-test-deploy/static/js/main.97f40dd6.chunk.js"
+  },
+  {
+    "revision": "f5d0cc9c770aa2081763",
+    "url": "https://zaka-invozone.github.io/react/build-test-deploy/static/js/runtime-main.a8a773e8.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://zaka-invozone.github.io/react/build-test-deploy/precache-manifest.0c0e3af54fdf91f64031f047e3712b67.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://zaka-invozone.github.io/react/build-test-deploy/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
