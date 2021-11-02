d0d81cba97937be258c417f416b4e182403e91f4
Zaka Rehman <m.zaka618@gmail.com>
2021-11-02 14:06:05 +0500

    test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React CI/CD
         </a>
       </header>
     </div>
